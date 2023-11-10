398f96fdaf98237e0b1cf6077228d7c2e08df2c1
acetrow <z.zairudin@lancaster.ac.uk>
2023-11-10 07:12:36 +0000

    Added Saturn ring and jupiter moon

---
+++ Asteroid.java
@@ -0,0 +1,16 @@
+public class Asteroid extends Planet 
+{
+    public Asteroid(double distance, double angle, double diameter, String col, double centreOfRotationDistance, double centreOfRotationAngle) 
+    {
+        super(distance, angle, diameter, col, centreOfRotationDistance, centreOfRotationAngle);
+    }
+    
+    public void draw(SolarSystem solarSystem) 
+    {
+
+        solarSystem.drawSolarObjectAbout(distance, angle, diameter, col, centreOfRotationDistance, centreOfRotationAngle);
+        angle = angle+2;
+ 
+
+    }
+}
--- Driver.java
+++ Driver.java
@@ -11,7 +11,8 @@ public class Driver
         Planet2 venus = new Planet2(145, 315, 32, "#c9b037", 0 ,0);
         Planet earth = new Planet(201, 270, 35, "#465A3D", 0 ,0);
         Planet mars = new Planet(241, 45, 20, "#800000", 0 ,0);
-        Planet2 jupiter = new Planet2(305, 135, 55, "#e3dccb", 0, 0);
+        Planet jupiter = new Planet(305, 135, 55, "#e3dccb", 0, 0);
+        Planet2 saturn_ring = new Planet2(359, 360, 45, "WHITE", 0, 0);
         Planet2 saturn = new Planet2(359, 360, 40, "#b5651d", 0, 0);
         Planet uranus = new Planet(411, 90, 30, "#ADD8E6", 0, 0);
         Planet2 neptune = new Planet2(450, 225, 25, "#00008B", 0, 0);
@@ -19,17 +20,19 @@ public class Driver
         Moon earthMoon = new Moon(30, 270, 2, "GREY", earth.distance, earth.angle);
         Moon marsMoon1 = new Moon(20, 45, 2, "GREY", mars.distance, mars.angle);
         Moon marsMoon2 = new Moon(15, 225, 2, "GREY", mars.distance, mars.angle);
+        Moon jupiterMoon = new Moon(31, 90, 3, "GREY", jupiter.distance, jupiter.angle);
+         
 
-        Comet[] comets = new Comet[144];
+        Asteroid[] asteroids = new Asteroid[144];
 
         int k = 0;
         for (int i = 0; i < 72; i++) {
-            comets[i] = new Comet(265, k, 1, "WHITE", 0, 0);
+            asteroids[i] = new Asteroid(265, k, 1, "WHITE", 0, 0);
             k = k+5;
         }
         double j = 2.5;
         for (int i = 72; i < 144; i++) {
-            comets[i] = new Comet(270, j, 1, "WHITE", 0, 0);
+            asteroids[i] = new Asteroid(270, j, 1, "WHITE", 0, 0);
             j = j+5;
         }
         
@@ -45,6 +48,7 @@ public class Driver
             earth.draw(solarSystem);
             mars.draw(solarSystem);
             jupiter.draw(solarSystem);
+            saturn_ring.draw(solarSystem);
             saturn.draw(solarSystem);
             uranus.draw(solarSystem);
             neptune.draw(solarSystem);
@@ -53,11 +57,12 @@ public class Driver
             earthMoon.draw(solarSystem);
             marsMoon1.draw(solarSystem);
             marsMoon2.draw(solarSystem);  
+            jupiterMoon.draw(solarSystem);  
 
-            //Comet
+            //Asteroid
             for (int i = 0; i < 144; i++) 
             {
-                comets[i].draw(solarSystem);
+                asteroids[i].draw(solarSystem);
             }
             
             solarSystem.finishedDrawing();
